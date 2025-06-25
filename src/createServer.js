78cd7f2eb78374bcd54cc124ad9e4f8c710fcc33
Iwantschenko <160021860+Iwantschenko@users.noreply.github.com>
2025-06-25 10:47:23 +0300

    finally

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,41 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
+const getParamsObj = (url) => {
+  const queryParams = {};
+
+  for (const [key, value] of url.searchParams.entries()) {
+    if (queryParams[key]) {
+      queryParams[key] = [].concat(queryParams[key], value);
+    } else {
+      queryParams[key] = value;
+    }
+  }
+
+  return queryParams;
+};
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const url = new URL(req.url, 'http://localhost:5700');
+
+    const resObject = {
+      parts: [
+        ...url.pathname
+          .slice(1)
+          .replace(/\/{2,}/g, '/')
+          .split('/'),
+      ],
+      query: getParamsObj(url),
+    };
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ ...resObject }));
+  });
+
+  return server;
 }
 
 module.exports = {
